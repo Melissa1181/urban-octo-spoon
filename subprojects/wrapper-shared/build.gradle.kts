2dd3b2e80bbbaa047418659cbeb7c4154863e45c
Melissa1181 <melissaannette1119@gmail.com>
2024-12-07 22:23:23 -0600

    Initialize patch-1

---
+++ subprojects/wrapper-shared/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("gradlebuild.distribution.api-java")
+}
+
+description = "Utility code shared between the wrapper and the Gradle distribution"
+
+gradlebuildJava.usedInWorkers()
+
+dependencies {
+
+    testImplementation(project(":base-services"))
+    testImplementation(project(":core-api"))
+    testImplementation(project(":native"))
+    testImplementation(libs.commonsCompress)
+
+    integTestImplementation(project(":dependency-management"))
+    integTestImplementation(project(":logging"))
+
+    integTestDistributionRuntimeOnly(project(":distributions-full"))
+}
